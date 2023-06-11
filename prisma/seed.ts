82dbd932aa3622ba7571f4b6112d225171ed953e
zk-glen <glenmaytum@gmail.com>
2023-06-11 03:23:31 +0100

    setup vercel postgres db and connect via prisma locally, setup seed

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { hash } from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+	const password = await hash("test", 12);
+	const user = await prisma.user.upsert({
+		where: {
+			email: "test@test.com",
+		},
+		update: {},
+		create: {
+			email: "test@test.com",
+			name: "Test User",
+			password,
+		},
+	});
+}
+
+main()
+	.then(() => prisma.$disconnect())
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
